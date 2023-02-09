7595798dce7cee8a79389089c1e902832e673656
Antonio-87 <davydenko.87@list.ru>
2023-02-09 19:09:07 +0300

    test

--- src/js/__tests__/orderByProps.test.js
+++ src/js/__tests__/orderByProps.test.js
@@ -1,16 +1,18 @@
-import orderByProps from "../orderByProps";
+import orderByProps from '../orderByProps';
 
 test.each([
     {
-      data: {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40},
-      expected: [
-        {key: "name", value: "мечник"},
-        {key: "level", value: 2},
-        {key: "attack", value: 80},
-        {key: "defence", value: 40},
-        {key: "health", value: 10},
-      ],
+        data: {
+            name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+        },
+        expected: [
+            { key: 'name', value: 'мечник' },
+            { key: 'level', value: 2 },
+            { key: 'attack', value: 80 },
+            { key: 'defence', value: 40 },
+            { key: 'health', value: 10 },
+        ],
     },
-    ])('order by props', ({ data, expected }) => {
-        expect(orderByProps(data, ["name", "level"])).toEqual(expected);
-    });
+])('order by props', ({ data, expected }) => {
+    expect(orderByProps(data, ['name', 'level'])).toEqual(expected);
+});
--- src/js/orderByProps.js
+++ src/js/orderByProps.js
@@ -1,24 +1,14 @@
-export default function orderByProps(obj, options=[]) {
-    let listKeysSort = Object.keys(obj).filter(el => !options.includes(el)).sort();
-    let firstProps = [];
-    let andProps = [];
-    if (options.length > 0) {
-       for (const key in options) {
-            Object.entries(obj).forEach(el => {
-                if (el[0] == key) {
-                    firstProps.push({key: el[0], value: el[1]})
-                }
-            });
-        } 
+export default function orderByProps(obj, options = []) {
+    const listKeysSort = Object.keys(obj).filter((el) => !options.includes(el)).sort();
+    const firstProps = [];
+    const endProps = [];
+    for (const prop in obj) {
+        if (!options.includes(prop)) {
+            endProps[listKeysSort.indexOf(prop)] = ({ key: prop, value: obj[prop] });
+        } else {
+            firstProps[options.indexOf(prop)] = ({ key: prop, value: obj[prop] });
+        }
     }
 
-    for (const key in listKeysSort) {
-        Object.entries(obj).forEach(el => {
-            if (el[0] == key) {
-                firstProps.push({key: el[0], value: el[1]})
-            }
-        });
-    }
-
-    return firstProps.concat(andProps);
+    return firstProps.concat(endProps);
 }
