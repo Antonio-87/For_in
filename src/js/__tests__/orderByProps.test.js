c0c568944c7722e7e130ec784d2d587047741cf3
Antonio-87 <davydenko.87@list.ru>
2023-02-09 21:26:35 +0300

    test

--- src/js/__tests__/orderByProps.test.js
+++ src/js/__tests__/orderByProps.test.js
@@ -16,3 +16,20 @@ test.each([
 ])('order by props', ({ data, expected }) => {
     expect(orderByProps(data, ['name', 'level'])).toEqual(expected);
 });
+
+test.each([
+    {
+        data: {
+            name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+        },
+        expected: [
+            { key: 'attack', value: 80 },
+            { key: 'defence', value: 40 },
+            { key: 'health', value: 10 },
+            { key: 'level', value: 2 },
+            { key: 'name', value: 'мечник' },
+        ],
+    },
+])('order by props (options = [])', ({ data, expected }) => {
+    expect(orderByProps(data, [])).toEqual(expected);
+});
